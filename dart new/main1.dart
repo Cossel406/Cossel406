6e28607b159f6b662e4d63d5161d3c74bc57bc32
Howard Kiprono <kipscossel@gmail.com>
2024-02-27 17:42:31 +0300

    latest files

---
+++ dart new/main1.dart
@@ -0,0 +1,22 @@
+void main() {
+// declaring variables
+  String name = "Kiprono"; //must be in quotes
+  String address = "Kenya"; //must be in quotes
+  num age = 21; //whole numbers
+  num height = 5.9; //decimal numbers
+  bool isMarried = false; //boolean
+
+// printing variables value
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+
+// This is a sinle line comment
+
+/* This is a multi-line comment
+This is another line in this comment
+
+*/
+}
